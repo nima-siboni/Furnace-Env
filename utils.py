33f15c801ffd7c99e2633cd9f79883faca7f8d88
nima-siboni <nima.siboni@gmail.com>
2023-12-27 04:22:41 +0100

    Revert "energy cost is corrected, some typings and docstrings are added."
    This reverts commit 06aef726c5168e5e253f5861173e2c4e427aca7e.

--- utils.py
+++ utils.py
@@ -8,29 +8,22 @@ from pydantic import BaseModel  # pylint: disable=no-name-in-module
 
 class FurnaceConfig(BaseModel):  # pylint: disable=too-few-public-methods
     """A class for furnace config."""
-    horizon: int  # time horizon of the environment
-    dimension: int  # dimensions of the environment's box
-    minimum_temperature: float  # minimum temperature of the environment
-    maximum_temperature: float  # maximum temperature of the environment
-    desired_volume_fraction: float  # desired volume fraction
-    temperature_change_per_step: float  # temperature change per step
-    number_of_pf_updates_per_step: int  # number of phase field updates per step
-    gamma: float  # interface energy constant
-    # termination pf change criterion, if the change
+    horizon: int
+    dimension: int
+    minimum_temperature: float
+    maximum_temperature: float
+    desired_volume_fraction: float
+    temperature_change_per_step: float
+    number_of_pf_updates_per_step: int
+    gamma: float
     termination_change_criterion: Optional[float]
-    # is less than this value, the episode terminates
-    # if true, the episode terminates if the temperature
     use_termination_temperature_criterion: bool
-    # goes out of the range [minimum_temperature, maximum_temperature]
-    mobility_type: str  # mobility type
-    g_list: list[int]  # list of g values
-    shift_pf: Optional[float] = 0.0  # a constant shift of pf values
-    initial_pf_variation: float  # initial pf variation
-    use_stop_action: bool  # if true, the agent can stop the furnace
-    # energy cost per step per degree above the room temperature
+    mobility_type: str
+    g_list: list[int]
+    shift_pf: Optional[float] = 0.0
+    initial_pf_variation: float
+    use_stop_action: bool
     energy_cost_per_step: float
-    room_temperature: Optional[float] = 22.0  # room temperature
-    # if true, the environment prints some information
     verbose: Optional[bool] = False
 
     class Config:  # pylint: disable=too-few-public-methods
